3bb702e31b753b0546bfb3dfc97da9a384cdd0aa
account <account@mail.com>
2024-10-10 00:31:09 +0300

    elgamal works

--- lab_3/src/lib.rs
+++ lab_3/src/lib.rs
@@ -1,10 +1,7 @@
 use std::hash::{DefaultHasher, Hasher};
-use num_integer::Roots;
-use num_prime::nt_funcs::is_prime64;
-use num_traits::Num;
-use num_traits::real::Real;
+use num_prime::nt_funcs::{is_prime64};
 use rand::Rng;
-use num_prime::RandPrime;
+use rand::distributions::uniform::SampleRange;
 
 fn calculate_hash(data: &[u8]) -> u16 {
     let mut hasher = DefaultHasher::new();
@@ -84,27 +81,22 @@ fn modpow(base: u64, exp: u64, modulus: u64) -> u64 {
     result
 }
 
-fn min_bits_number(number: u64) -> u8 {
-    const MASK: u64 = 1 << 63;
-    let mut number = number;
-    let mut zeros = 0u8;
-    while number & MASK == 0 {
-        number <<= 1;
-        zeros += 1;
+fn gen_prime<R: SampleRange<u16> + Clone>(r: R) -> u16 {
+    loop {
+        let n = rand::thread_rng().gen_range(r.clone());
+        if is_prime64(n as u64) {
+            break n;
+        }
     }
-    64 - zeros
 }
 
 mod dsa {
     use std::ops::Rem;
-    use num_bigint::{BigUint, RandBigInt};
-    use num_prime::RandPrime;
-    use num_traits::{One, Pow, ToPrimitive, Zero};
     use rand::Rng;
-    use crate::{calculate_hash, modinv, modpow};
+    use crate::{calculate_hash, gen_prime, modinv, modpow};
     use num_prime::nt_funcs::is_prime64;
 
-    struct DSA {
+    pub struct Dsa {
         q: u16,
         p: u64,
         g: u64,
@@ -112,23 +104,18 @@ mod dsa {
         y: u64,
     }
 
-    struct Signature {
+    pub struct Signature {
         r: u16,
         s: u16,
     }
 
-    impl DSA {
+    impl Dsa {
         pub fn new() -> Self {
             let (q, p) = 'qp_loop: loop {
                 const N: usize = 16;
                 const MIN: u16 = (1 << (N - 1)) as u16;
                 const MAX: u16 = ((1 << N) - 1) as u16;
-                let q = loop {
-                    let q = rand::thread_rng().gen_range(MIN..MAX);
-                    if is_prime64(q as u64) {
-                        break q;
-                    }
-                };
+                let q = gen_prime(MIN..MAX);
                 let mut p_1 = (q as u64) * 2;
                 loop {
                     if u64::MAX - p_1 < q as u64 {
@@ -173,34 +160,127 @@ mod dsa {
         }
     }
 
-    pub fn verify(dsa: &DSA, signature: &Signature, data: &[u8]) -> bool {
+    pub fn verify(dsa: &Dsa, signature: &Signature, data: &[u8]) -> bool {
         if ![signature.s, signature.r].iter().all(|num| {
             *num > 0 && *num < dsa.q
         }) {
             return false;
         }
-
-        let w = modinv(signature.s, dsa.q).unwrap();
-        let data_hash = calculate_hash(data);
-        let u_one = (data_hash as u64 * w as u64).rem(dsa.q as u64) as u16;
-        let u_two = (signature.r as u64 * w as u64).rem(dsa.q as u64) as u16;
-        let v_one = modpow(dsa.g, u_one as u64, dsa.p) as u128;
-        let v_two = modpow(dsa.y, u_two as u64, dsa.p) as u128;
-        let v = ((((v_one * v_two) % dsa.p as u128) as u64) % dsa.q as u64) as u16;
-        v == signature.r
+        if let Some(w) = modinv(signature.s, dsa.q) {
+            let data_hash = calculate_hash(data);
+            let u_one = (data_hash as u64 * w as u64).rem(dsa.q as u64) as u16;
+            let u_two = (signature.r as u64 * w as u64).rem(dsa.q as u64) as u16;
+            let v_one = modpow(dsa.g, u_one as u64, dsa.p) as u128;
+            let v_two = modpow(dsa.y, u_two as u64, dsa.p) as u128;
+            let v = ((((v_one * v_two) % dsa.p as u128) as u64) % dsa.q as u64) as u16;
+            v == signature.r
+        } else {
+            false
+        }
     }
 
     #[cfg(test)]
     mod tests {
-        use crate::{dsa};
 
         #[test]
         fn it_works() {
             let message = "message";
-            let dsa = dsa::DSA::new();
+            let dsa = super::Dsa::new();
             let signature = dsa.sign(message.as_bytes());
-            let res = dsa::verify(&dsa, &signature, message.as_bytes());
+            let res = super::verify(&dsa, &signature, message.as_bytes());
             assert!(res);
+        }
+    }
+}
+
+mod elgamal {
+    use num_traits::ToPrimitive;
+    use rand::Rng;
+    use thiserror::Error;
+    use crate::{gen_prime, modinv, modpow};
+    use crate::elgamal::DecipherError::CanNotConvertToByte;
+
+    pub struct CipheredData {
+        a: u16,
+        bs: Vec<u16>
+    }
+
+    const MIN_P: u16 = u8::MAX as u16 + 1;
+    const MAX_P: u16 = u16::MAX;
+
+    pub struct PublicKey {
+        g: u16,
+        y: u16,
+        p: u16,
+    }
+
+    impl PublicKey {
+        pub fn cipher(&self, data: &[u8]) -> CipheredData {
+            let (a, u) = {
+                let k = rand::thread_rng().gen_range(1..(self.p - 1));
+                (
+                    modpow(self.g as u64, k as u64, self.p as u64) as u16,
+                    modpow(self.y as u64, k as u64, self.p as u64) as u16
+                )
+            };
+            CipheredData {
+                a,
+                bs: data.iter()
+                    .map(|m| {
+                        ((u as u32 * (*m as u32)) % self.p as u32) as u16
+                    })
+                    .collect()
+            }
+        }
+    }
+
+    #[derive(Error, Debug)]
+    pub enum DecipherError {
+        #[error("Can not create inverse")]
+        CanNotCreateInverse{ s: u16, p: u16 },
+        #[error("Can not convert to byte")]
+        CanNotConvertToByte(u16),
+    }
+
+    pub struct PrivateKey {
+        p: u16,
+        x: u16,
+    }
+
+    impl PrivateKey {
+        pub fn decipher(&self, c_data: &CipheredData) -> Result<Vec<u8>, DecipherError> {
+            let s_inv = {
+                let s = modpow(c_data.a as u64, self.x as u64, self.p as u64) as u16;
+                modinv(s, self.p).ok_or(DecipherError::CanNotCreateInverse { s, p: self.p })?
+            };
+            c_data.bs.iter().try_fold(Vec::new(), |mut acc, c| {
+                let prob_byte = ((*c as u32 * s_inv as u32) % self.p as u32) as u16;
+                let byte = prob_byte.to_u8().ok_or(CanNotConvertToByte(prob_byte))?;
+                acc.push(byte);
+                Ok(acc)
+            })
+        }
+    }
+
+    pub fn create_keys() -> (PublicKey, PrivateKey) {
+        let p = gen_prime(MIN_P..MAX_P);
+        let g = gen_prime(1..p);
+        let x = rand::thread_rng().gen_range(1..(p - 2));
+        let y = modpow(g as u64, x as u64, p as u64) as u16;
+        (PublicKey { g, y, p }, PrivateKey { p, x })
+    }
+
+    #[cfg(test)]
+    mod tests {
+
+        #[test]
+        fn it_works() {
+            let message = "message";
+            let (public_key, private_key) = super::create_keys();
+            let c_data = public_key.cipher(message.as_bytes());
+            let data = private_key.decipher(&c_data).unwrap();
+            let deciphered_message = String::from_utf8(data).unwrap();
+            assert_eq!(message, deciphered_message);
         }
     }
 }

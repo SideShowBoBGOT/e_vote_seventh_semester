c9bcb1cfb93bef0302181c3912e3a320b33e300c
account <account@mail.com>
2024-10-22 10:55:01 +0300

    code done

--- lab_6/src/lib.rs
+++ lab_6/src/lib.rs
@@ -63,8 +63,6 @@ mod elgamal {
     use num_traits::{Pow, ToPrimitive};
     use serde::{Deserialize, Serialize};
     use thiserror::Error;
-    use num_prime::nt_funcs::is_prime64;
-    use rand::distributions::uniform::SampleRange;
     use rand::Rng;
     use crate::alg_utils;
     use crate::alg_utils::{modinv, positive_mod, ModInvError};
@@ -149,7 +147,6 @@ mod elgamal {
     pub struct PrivateKey {
         p: u16,
         g: u16,
-        y: u16,
         x: u16,
     }
 
@@ -201,7 +198,7 @@ mod elgamal {
             let g = alg_utils::gen_prime(1..p);
             let x = rand::thread_rng().gen_range(1..(p - 2));
             let y = modpow(g as u64, x as u64, p as u64) as u16;
-            Self { public_key: PublicKey { g, y, p }, private_key: PrivateKey { p, g, y, x } }
+            Self { public_key: PublicKey { g, y, p }, private_key: PrivateKey { p, g, x } }
         }
     }
 
@@ -244,8 +241,8 @@ mod bbs {
     use getset::Getters;
     use num_integer::{lcm, gcd};
     use rand::Rng;
-    use crate::alg_utils::{gen_prime, modinv};
-    use crate::elgamal::{modpow, CipheredData};
+    use crate::alg_utils::{gen_prime};
+    use crate::elgamal::{modpow};
 
     #[derive(Getters)]
     #[get = "pub with_prefix"]
@@ -254,16 +251,18 @@ mod bbs {
         private_key: PrivateKey,
     }
 
+    #[derive(Copy, Clone)]
     pub struct PrivateKey {
         p: u16,
         q: u16,
     }
 
+    #[derive(Copy, Clone)]
     pub struct PublicKey(u32);
 
-    impl Default for KeyPair {
+    impl KeyPair {
 
-        fn default() -> Self {
+        pub fn new() -> Self {
             fn gen_prime_34() -> u16 {
                 loop {
                     let p = gen_prime(u8::MAX as u16..u16::MAX);
@@ -369,11 +368,175 @@ mod bbs {
         fn it_works() {
             let message = "message";
             let mut c_message = bincode::serialize(&message).unwrap();
-            let key_pair = super::KeyPair::default();
+            let key_pair = super::KeyPair::new();
             let x_0 = key_pair.public_key.cipher(&mut c_message);
             key_pair.private_key.decipher(&mut c_message, x_0);
             let original_message: String = bincode::deserialize(&c_message).unwrap();
             assert_eq!(original_message, message);
+        }
+    }
+}
+
+mod sim_env {
+
+    mod reg_bureau {
+        use rand::Rng;
+        use serde::{Deserialize, Serialize};
+        use crate::sim_env::ec::PublicToken;
+
+        type InnerId = u16;
+
+        #[derive(Serialize, Deserialize, Debug, Copy, Clone, PartialEq, Eq, Hash)]
+        pub struct Id(InnerId);
+
+
+        pub struct RegBureau {
+            public_tokens: Vec<PublicToken>
+        }
+
+        pub fn send_ids(ids_len: u16) -> Vec<Id> {
+            let mut ids = Vec::with_capacity(ids_len as usize);
+            for i in 0..ids_len {
+                loop {
+                    let id = Id(rand::thread_rng().gen_range(0..InnerId::MAX));
+                    if !ids.contains(&id) {
+                        ids.push(id);
+                        break;
+                    }
+                }
+            }
+            ids
+        }
+    }
+
+    mod ec {
+        use std::collections::HashMap;
+        use num_integer::Integer;
+        use serde::{Deserialize, Serialize};
+        use crate::{bbs, elgamal};
+        use crate::elgamal::CipheredData;
+        use crate::sim_env::reg_bureau::Id;
+        use crate::sim_env::voters::VoteData;
+
+        pub struct PublicToken {
+            pub id: Id,
+            pub public_key: bbs::PublicKey
+        }
+
+        #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Hash, Clone, Copy)]
+        pub struct CandidateId(u16);
+
+        pub struct Ec {
+            elgamal: elgamal::KeyPair,
+            candidates: HashMap<CandidateId, u64>,
+            private_tokens: HashMap<Id, bbs::PrivateKey>,
+
+        }
+
+        impl Ec {
+
+            pub fn accept_ids_generate_tokens(
+                ids: &[Id],
+                candidates_len: u16
+            ) -> (Self, Vec<PublicToken>) {
+                let mut public_tokens = Vec::with_capacity(ids.len());
+                let mut private_tokens = HashMap::with_capacity(ids.len());
+                for id in ids {
+                    let key_pair = bbs::KeyPair::new();
+                    public_tokens.push(PublicToken {
+                        id: *id,
+                        public_key: *key_pair.get_public_key()
+                    });
+                    private_tokens.insert(*id, *key_pair.get_private_key());
+                }
+                (
+                    Self {
+                        elgamal: elgamal::KeyPair::default(),
+                        candidates: HashMap::from_iter(
+                            (0..candidates_len).map(|i| (CandidateId(i), 0u64))
+                        ),
+                        private_tokens
+                    },
+                    public_tokens
+                )
+            }
+
+            pub fn get_public_key(&self) -> &elgamal::PublicKey {
+                &self.elgamal.public_key
+            }
+
+            pub fn get_candidates(&self) -> impl Iterator<Item=&CandidateId> {
+                self.candidates.keys()
+            }
+
+            pub fn get_candidate_statistic(&self) -> impl Iterator<Item=(&CandidateId, &u64)> {
+                self.candidates.iter()
+            }
+
+            pub fn accept_vote(&mut self, ciphered_data: CipheredData) {
+                let cand_id: CandidateId = {
+                    let data = self.elgamal.private_key.decipher(&ciphered_data).unwrap();
+                    let mut vote_data: VoteData = bincode::deserialize(&data).unwrap();
+                    let private_key = self.private_tokens.get(&vote_data.id).unwrap();
+                    private_key.decipher(&mut vote_data.data, vote_data.x_0);
+                    bincode::deserialize(&vote_data.data).unwrap()
+                };
+                self.candidates.get_mut(&cand_id).unwrap().inc();
+            }
+        }
+    }
+
+    mod voters {
+        use rand::prelude::{IteratorRandom, SliceRandom};
+        use serde::{Deserialize, Serialize};
+        use crate::sim_env::ec;
+        use crate::sim_env::ec::PublicToken;
+        use crate::sim_env::reg_bureau::Id;
+
+        #[derive(Debug, Serialize, Deserialize)]
+        pub struct VoteData {
+            pub id: Id,
+            pub x_0: u32,
+            pub data: Vec<u8>
+        }
+
+        pub fn vote(reg_bureau_tokens: Vec<PublicToken>, ec: &mut ec::Ec) {
+            reg_bureau_tokens.into_iter().for_each(|token| {
+                let candidate = ec.get_candidates()
+                    .choose(&mut rand::thread_rng()).unwrap();
+                let mut data = bincode::serialize(candidate).unwrap();
+
+                let x_0 = token.public_key.cipher(&mut data);
+                let ser_vote_data = bincode::serialize(
+                    &VoteData { id: token.id, x_0, data }
+                ).unwrap();
+                let cipher_data = ec.get_public_key().cipher(&ser_vote_data);
+                ec.accept_vote(cipher_data);
+            });
+        }
+    }
+
+
+    #[cfg(test)]
+    mod tests {
+        use crate::sim_env::{ec, reg_bureau, voters};
+
+        #[test]
+        fn it_works() {
+
+
+            let (mut ec, reg_bureau_tokens) = {
+                ec::Ec::accept_ids_generate_tokens(
+                    &reg_bureau::send_ids(20),
+                    5
+                )
+            };
+
+            voters::vote(reg_bureau_tokens, &mut ec);
+
+            for p in ec.get_candidate_statistic() {
+                println!("{:?}", p);
+            }
         }
     }
 }
